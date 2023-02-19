e821e079f47784cec8cf91973578f11fd760980e
CrzTosheba <fugu@list.ru>
2023-02-19 14:07:49 +0300

    Добавили скрипты. Задание 29.7

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,97 @@
+﻿/*
+* Функция для фильтрации контента
+* Будет вызываться благодаря атрибуту oninput на index.html
+*
+* */
+function filterContent(userInput) { // Принимаем пользовательский ввод в качестве параметра.
+
+    // Находим контейнеры с видео, которые необходимо фильтровать
+    let elements = document.getElementsByClassName('video-container');
+
+    // Пробегаемся по контейнерам
+    for (let i = 0; i <= elements.length; i++) {
+        // Вытаскиваем текст описания видео, которое необходимо отфильтровать
+        let videoText = elements[i].querySelector(".video-title").innerText;
+
+
+        // Выполняем фильтрацию, сравнивая значения в нижнем регистре
+        if (!videoText.toLowerCase().includes(userInput.toLowerCase())) {
+            // Скрываем неподходящие
+            elements[i].style.display = 'none';
+        } else {
+            // Показываем подходящие
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
+// Логирование сессии (объявлено через function declaration)
+let sessionLog = function logSession(session) {
+    // Вывод в консоль
+    for (let result of session) {
+        console.log(result)
+    }
+}
+// Обработка сессии (объявлено через expression)
+function handleSession() {
+    // создадим объект Map для хранения сессии
+    let session = new Map();
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
+
+    // Запросим возраст пользователя и тоже сохраним
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+
+    // Проверка на возраст и сохранение сессии
+    if (session.get("age") >= 18) {
+        let startDate = new Date().toLocaleString();
+
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+        session.set("startDate", startDate)
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+
+    // Теперь мы возвращаем объект сессии
+    return session;
+}
+/*
+* Запросим пользовательский ввод
+* и сохраним отзыв в объект
+*
+* */
+/*
+* Запишем отзыв на страницу
+*
+* */
+
+function getReview() {
+    // Создадим объект
+    let review = {}
+
+    // Сохраним свойство имени
+    review["userName"] = prompt("Как вас зовут ?")
+    if (review["userName"] == null) {
+        return
+    }
+
+    // Сохраним текст отзыва
+    review["comment"] = prompt("Напишите свой отзыв")
+    if (review["comment"] == null) {
+        return
+    }
+
+    // Сохраним текущее время
+    review["date"] = new Date().toLocaleString()
+
+    // Добавим на страницу
+    writeReview(review)
+
+}
+const writeReview = review => {
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
+        `<p>${review['comment']}</p>` +
+        '</div>';
+}
